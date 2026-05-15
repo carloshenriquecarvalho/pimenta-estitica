de0032b236b6f85820d37debb3a556984cc5afe9
carloscarvalho <misterchaty@outlook.com>
2026-05-15 09:55:00 -0300

    Fix Performance Issues

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google";
-
+import Script from "next/script";
 
 const inter = Inter({
   variable: "--font-inter",
@@ -29,8 +28,24 @@ export default function RootLayout({
       lang="pt-BR"
       className={`${inter.variable} ${playfair.variable} h-full antialiased`}
     >
-      <GoogleTagManager gtmId="GTM-5LH3THT6" />
-      <body className="min-h-full flex flex-col">{children}</body>
+      <head>
+        <link rel="preconnect" href="https://cdn.jsdelivr.net" crossOrigin="anonymous" />
+        <Script id="gtm" strategy="lazyOnload">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-5LH3THT6');
+          `}
+        </Script>
+      </head>
+      <body className="min-h-full flex flex-col">
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-5LH3THT6" height="0" width="0" style={{ display: 'none', visibility: 'hidden' }}></iframe>
+        </noscript>
+        {children}
+      </body>
     </html>
   );
 }
